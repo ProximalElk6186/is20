e45858153c740a405da2e5fea2d1e52ffbc37ce9
ProximalElk6186 <133893464+ProximalElk6186@users.noreply.github.com>
2024-11-09 12:34:23 +0200

    Added a bit of comments

--- Program.cs
+++ Program.cs
@@ -1,23 +1,42 @@
-﻿internal class Program
+﻿internal class Program  // A standart class name
 {
     private static void Main(string[] args)
+//A private static void Main with a string array 'args' containing arguments the user passed to the programme 
     {
-        if (args.Length == 0 || args.Length > 1)
+            // Checking if arguments quantity is exactly 1
+        if (args.Length == 0 || args.Length > 1)    
         {
-            Console.WriteLine("Usage: [number]");
+            // User is a dumbass, tell him this
+            Console.WriteLine("Usage: [number]");   
         }
-        else if (args.Length == 1)
+        // If user is not a dumbass... (pretty much impossible)
+        else if (args.Length == 1)  
+
         {
+            // Returns:
+            // isNumber20
+            // With an argument as a parameter
+            // Converted to string
             Console.WriteLine(isNumber20(args[0]).ToString());
         }
     }
-    public static bool isNumber20(string no) 
+    // Does very hard mathematic calculations to check
+    // if the number the user passed to the programme
+    // as an argument is 20 or not
+    public static bool isNumber20(string no)    
     {
+        // Switch that actually does the shit
+        // we're here for
         switch (no) 
         {
+            // return "false" by default
             default:
+                // returning "false" by default
                 return false;
+            // if the number is 20... (mostly impossible)
             case "20":
+                // return "true" if the number is 20
+                // (mostly impossible)
                 return true;
         }
     }
