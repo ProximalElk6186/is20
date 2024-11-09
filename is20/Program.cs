15e87743a939d8be2dc7da32e901bea95a404af7
ProximalElk6186 <133893464+ProximalElk6186@users.noreply.github.com>
2024-11-09 12:20:34 +0200

    Initial write-up

---
+++ is20/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        if (args.Length == 0 || args.Length > 1)
+        {
+            Console.WriteLine("Usage: [number]");
+        }
+        else if (args.Length == 1)
+        {
+            Console.WriteLine(isNumber20(args[0]).ToString());
+        }
+    }
+    public static bool isNumber20(string no) 
+    {
+        switch (no) 
+        {
+            default:
+                return false;
+            case "20":
+                return true;
+        }
+    }
+}
