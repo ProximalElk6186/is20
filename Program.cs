a9951f29d48a7bca2d4aea2700e7f0eee2ab358a
ProximalElk6186 <133893464+ProximalElk6186@users.noreply.github.com>
2025-04-28 13:15:05 +0300

    Full rewrite

--- Program.cs
+++ Program.cs
@@ -1,42 +1,34 @@
-﻿internal class Program  // A standart class name
+internal class Program // A standard class name for a programme of this caliber
 {
     private static void Main(string[] args)
-//A private static void Main with a string array 'args' containing arguments the user passed to the programme 
+    // A private static void Main with a string array 'args' containing arguments passed by the user
     {
-            // Checking if arguments quantity is exactly 1
-        if (args.Length == 0 || args.Length > 1)    
+        // Check if the number of arguments is exactly 1
+        if (args.Length == 0 || args.Length > 1)
         {
-            // User is a dumbass, tell him this
-            Console.WriteLine("Usage: [number]");   
+            // User failed to follow basic instructions, remind them politely
+            Console.WriteLine("Usage: [number]");
         }
-        // If user is not a dumbass... (pretty much impossible)
-        else if (args.Length == 1)  
-
+        else if (args.Length == 1)
         {
-            // Returns:
-            // isNumber20
-            // With an argument as a parameter
-            // Converted to string
+            // If the user miraculously passed exactly one argument, proceed
+            // Display whether the input is "20" (a rare event)
             Console.WriteLine(isNumber20(args[0]).ToString());
         }
     }
-    // Does very hard mathematic calculations to check
-    // if the number the user passed to the programme
-    // as an argument is 20 or not
-    public static bool isNumber20(string no)    
+
+    // Performs highly sophisticated operations to determine
+    // if the provided input is the number 20
+    public static bool isNumber20(string no)
     {
-        // Switch that actually does the shit
-        // we're here for
-        switch (no) 
+        // Switch statement to elegantly handle this extremely complex task
+        switch (no)
         {
-            // return "false" by default
             default:
-                // returning "false" by default
+                // If the input is anything other than "20", return false
                 return false;
-            // if the number is 20... (mostly impossible)
             case "20":
-                // return "true" if the number is 20
-                // (mostly impossible)
+                // If, against all odds, the input is "20", return true
                 return true;
         }
     }
